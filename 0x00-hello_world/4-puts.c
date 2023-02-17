67da31814a6ca739c0cf136deacb31331fb7efee
mustafa <mustafaabdulkhailq49@gmail.com>
2023-02-16 16:25:45 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 int main(void)
 {
 /**
-*  main - print a given phrase
-*Return : 0 if exited properly, non-zero otherwise
+ * main - print a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
 */
-	puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
