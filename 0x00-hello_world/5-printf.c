cf2a033fe2a007e142af8a4b0483a9fe6f0dd306
mustafa <mustafaabdulkhailq49@gmail.com>
2023-02-19 09:08:09 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+   *  main - print a given phrase
+   * Return: 0 if exited properly, non-zero otherwise
+   */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+
+}
